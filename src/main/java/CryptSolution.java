d713cb5e1616a6d4dad7b488c898fe8689d6dce3
Mohit Keswani <mohit310@users.noreply.github.com>
2021-11-14 20:13:26 -0500

    New addition

--- src/main/java/CryptSolution.java
+++ src/main/java/CryptSolution.java
@@ -23,10 +23,10 @@ public class CryptSolution {
     }
 
     public static void main(String[] args) {
-        //System.out.println(CryptSolution.isCryptSolution(new String[]{"SEND", "MORE", "MONEY"}, new char[][]{{'O', '0'}, {'M', '1'}, {'Y', '2'}, {'E', '5'}, {'N', '6'},
-        //        {'D', '7'}, {'R', '8'}, {'S', '9'}}));
-        //System.out.println(CryptSolution.isCryptSolution(new String[]{"TEN", "TWO", "ONE"}, new char[][]{{'O', '1'}, {'T', '0'}, {'W', '9'}, {'E', '5'}, {'N', '4'}}));
-        //System.out.println(CryptSolution.isCryptSolution(new String[]{"A", "A", "A"}, new char[][]{{'A', '0'}}));
+        System.out.println(CryptSolution.isCryptSolution(new String[]{"SEND", "MORE", "MONEY"}, new char[][]{{'O', '0'}, {'M', '1'}, {'Y', '2'}, {'E', '5'}, {'N', '6'},
+                {'D', '7'}, {'R', '8'}, {'S', '9'}}));
+        System.out.println(CryptSolution.isCryptSolution(new String[]{"TEN", "TWO", "ONE"}, new char[][]{{'O', '1'}, {'T', '0'}, {'W', '9'}, {'E', '5'}, {'N', '4'}}));
+        System.out.println(CryptSolution.isCryptSolution(new String[]{"A", "A", "A"}, new char[][]{{'A', '0'}}));
         System.out.println(CryptSolution.isCryptSolution(new String[]{"AAAAAAAAAAAAAA", "BBBBBBBBBBBBBB", "CCCCCCCCCCCCCC"}, new char[][]{{'A', '1'}, {'B', '2'}, {'C', '3'}}));
     }
 }
